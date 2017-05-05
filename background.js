1c075a0a13ce68e6a01b17ee05beb8d206b837e4
kentloog <kentloog@greeny.cs.tlu.ee>
2017-05-05 09:53:16 +0300

    Valmis

---
+++ background.js
@@ -0,0 +1,76 @@
+var s;
+var start;
+var kokku=0;
+var timeOut = 3*60;
+var secondsCounter = 0;
+var end;
+var totaltime = 0;
+
+window.onload = function (){
+  console.log(localStorage.getItem("timerSec"));
+  if(localStorage.getItem("timerSec") === null){
+    localStorage.setItem("timerSec", 0);
+  } else {
+    totaltime = parseInt(localStorage.getItem("timerSec"));
+  }
+startTimer();
+  var myVar = setInterval(timedata, 1000);
+};
+
+function timedata(){
+        end = performance.now();
+        //totaltime = parseInt(localStorage.getItem("timerSec"));
+        totaltime = totaltime + (end-start)/1000;
+        start = performance.now();
+
+        console.log("totaltime: "+ totaltime);
+        localStorage.setItem("timerSec", totaltime);
+
+
+        if(totaltime > 20*60){
+          kokku  += totaltime;
+          console.log("Kokku: "+ kokku);
+
+          alert();
+        }
+
+
+}
+
+function alert(){
+  console.log("Time's up!");
+  totaltime = 0;
+  confirm("Puhka silmi 1 minut! Seejärel vajuta OK, et jätkata.");
+  startTimer();
+}
+
+function startTimer(){
+    start = performance.now();
+    return start;
+}
+
+document.onclick = function() {
+    secondsCounter = 0;
+};
+document.onmousemove = function() {
+    secondsCounter = 0;
+};
+document.onkeypress = function() {
+    secondsCounter = 0;
+};
+
+window.setInterval(checkTime, 1000);
+
+function checkTime() {
+    secondsCounter++;
+
+    if (secondsCounter >= timeOut) {
+        kokku  += totaltime - timeOut;
+        console.log("User inactive -> timer reset");
+        console.log("Kokku: " +kokku);
+        secondsCounter = 0;
+        totaltime = 0;
+        confirm("Olid mõnda aega eemal, vajuta OK kui oled tagasi.");
+        startTimer();
+    }
+}
