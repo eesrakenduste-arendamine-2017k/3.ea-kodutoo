5598aa2cd1ab0b2d793832a3921af3929085b857
hensav <hensav@gmail.com>
2017-05-25 15:39:29 +0300

    fetching data from Discogs API

--- README.md
+++ README.md
@@ -2,18 +2,8 @@
 
 1. Youtubes loo pealkirjale vajutades sooritatakse päring Discogsi (plaaditurg) antud loo kohta, vastustega leht avatakse
  uues aknas.
-2. Logib otsingu tulemused andmebaasi
+2. Info kuvamine kasutades Discogsi APIt
+3. Logib otsingu tulemused andmebaasi
 
-3. Võimalikud edasiarendused:(võivad kujuneda keeruliseks kuna ühel plaadil on avaliselt mitu erinevat väljalaset)
--Nupp antud plaadi wishlisti lisamiseks
--Statistika kuvamine nt:
- Have: 67
- Want: 333
- Avg Rating: 4.7 / 5
- Ratings: 33
- Last Sold: 18 Feb 17
- Lowest: €10.98
- Median: €31.22
- Highest: €85.00
 
 [![https://gyazo.com/0d63aca32f18b3e48ac3ef80119698da](https://i.gyazo.com/0d63aca32f18b3e48ac3ef80119698da.gif)](https://gyazo.com/0d63aca32f18b3e48ac3ef80119698da)
--- background.js
+++ background.js
@@ -3,8 +3,12 @@ document.addEventListener("DOMContentLoaded", process);
 document.onload = process();
 function process() {
     console.log("loaded extension!");
-    var element = document.getElementById("eow-title");
-    var elementTitle = document.getElementById("eow-title").title;
+    let div = document.createElement('div');
+    div.id = 'discogs';
+    div.style.padding = "10px 10px 10px 0px";
+    document.getElementById("watch7-user-header").appendChild(div);
+    let element = document.getElementById("eow-title");
+    let elementTitle = document.getElementById("eow-title").title;
     //on hover change cursor to pointer
     element.style.cursor = 'pointer';
     //on mouse over change color to gray
@@ -15,15 +19,35 @@ function process() {
     element.onmouseout = function () {
         element.style.color = 'black';
     };
-    var stripedTitle = elementTitle.replace(/ *\([^)]*\) */g, "");
+    let stripedTitle = elementTitle.replace(/ *\([^)]*\) */g, "");
     //Discogs search
-    var URL = "https://www.discogs.com/search/?q=" + stripedTitle;
+    let URL = "https://www.discogs.com/search/?q=" + stripedTitle;
     element.addEventListener("click", function(){
         window.open(URL);
     });
+    fetch('https://api.discogs.com/database/search?q=' + elementTitle + '&token=MgXEXtEsiKFHKaOPPQtzPraAOhHDxMHWXHxBgQrb')
+        .then(res => res.json())
+        .then(function(data) {
+            let query = (data)
+            let id = query.results[0].id
+            fetch('https://api.discogs.com/releases/'+id+'?eur')
+                .then(res => res.json())
+                .then(function(data) {
+                    console.log(id)
+                    let masterQuery = (data)
+                    let lowestPrice = masterQuery.lowest_price
+                    let style = masterQuery.styles[0]
+                    let year = masterQuery.year
+                    let have = masterQuery.community.have
+                    let want = masterQuery.community.want
+                    let country = masterQuery.country
+                    div.innerHTML = "<ul><li>Want : "+want+"</li><li>Have: "+have+"</li><li>Lowest price: "+lowestPrice+"€"+
+                        "</li><li>Country: "+country+"</li><li>Style: "+style+"</li><li> Year: "+year+"</li></ul>"
+                })
+        });
     firebase.initializeApp(config);
-    var id = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-        var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
+    let id = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+        let r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
         return v.toString(16);
     });
     firebase.database().ref('discogs/' + id).set({
