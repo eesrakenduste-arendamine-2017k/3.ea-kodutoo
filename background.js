35361035b7cacf3cda33d6a8c9c6dedc01f2fd90
Tõnis Nerep <tonis@adm.ee>
2017-05-29 12:30:41 +0300

    Save validated url-s to database
    Firebase integration

--- background.js
+++ background.js
@@ -1,45 +1,102 @@
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
+firebase.initializeApp(config);
 
-/*
-  Displays a notification with the current time. Requires "notifications"
-  permission in the manifest file (or calling
-  "Notification.requestPermission" beforehand).
-*/
+var database = firebase.database();
+var user;
+
+
+function writeValidationData(url) {
+    var time = timeStamp();
+    firebase.database().ref('pages/' + user).set({
+        user: user,
+        testedUrl: url,
+        time: time
+    });
+
+}
+
+function getUserName() {
+
+    user = prompt("Tell me your name", "");
+
+}
+
+function getDataFromFirebase() {
+    return firebase.database().ref('/pages/').once('value').then(function(snapshot) {
+        var data = snapshot.val();
+        var fbData = JSON.parse(JSON.stringify(data));
+        for (var key in data) {
+            if (data.hasOwnProperty(key)) {
+                document.getElementById('data').innerHTML +=
+                    '<li><p id="user"><strong>User: </strong>' + key + '</p><ul><li id="url"><strong>Url: </strong>' + fbData[key].testedUrl + '</li> <li id="time"><strong>Time: </strong>' + fbData[key].time + '</li></ul></li>';
+            }
+        }
+    });
+
+}
+
+function timeStamp() {
+    // Create a date object with the current time
+    var now = new Date();
+
+    // Create an array with the current month, day and time
+    var date = [now.getMonth() + 1, now.getDate(), now.getFullYear()];
+
+    // Create an array with the current hour, minute and second
+    var time = [now.getHours(), now.getMinutes(), now.getSeconds()];
+
+    // Determine AM or PM suffix based on the hour
+    var suffix = (time[0] < 12) ? "AM" : "PM";
+
+    // Convert hour from military time
+    time[0] = (time[0] < 12) ? time[0] : time[0] - 12;
+
+    // If hour is 0, set it to 12
+    time[0] = time[0] || 12;
+
+    // If seconds and minutes are less than 10, add a zero
+    for (var i = 1; i < 3; i++) {
+        if (time[i] < 10) {
+            time[i] = "0" + time[i];
+        }
+    }
+
+    // Return the formatted string
+    return date.join("/") + " " + time.join(":") + " " + suffix;
+}
+
 function show() {
-  var time = /(..)(:..)/.exec(new Date());     // The prettyprinted time.
-  var hour = time[1] % 12 || 12;               // The prettyprinted hour.
-  var period = time[1] < 12 ? 'a.m.' : 'p.m.'; // The period of the day.
-  new Notification(hour + time[2] + ' ' + period, {
-    icon: 'eye.png',
-    body: 'Take 2 minutes and rest your eyes from computer screen.'
-  });
+    var time = /(..)(:..)/.exec(new Date()); // The prettyprinted time.
+    var hour = time[1] % 12 || 12; // The prettyprinted hour.
+    var period = time[1] < 12 ? 'a.m.' : 'p.m.'; // The period of the day.
+    new Notification(hour + time[2] + ' ' + period, {
+        icon: 'eye.png',
+        body: 'Take 2 minutes and rest your eyes from computer screen.'
+    });
 }
 
 // Conditionally initialize the options.
 if (!localStorage.isInitialized) {
-  localStorage.isActivated = true;   // The display activation.
-  localStorage.frequency = 1;        // The display frequency, in minutes.
-  localStorage.isInitialized = true; // The option initialization.
+    localStorage.isActivated = true; // The display activation.
+    localStorage.frequency = 1; // The display frequency, in minutes.
+    localStorage.isInitialized = true; // The option initialization.
 }
 
 // Test for notification support.
 if (window.Notification) {
-  // While activated, show notifications at the display frequency.
-  if (JSON.parse(localStorage.isActivated)) { show(); }
+    // While activated, show notifications at the display frequency.
+    if (JSON.parse(localStorage.isActivated)) { show(); }
 
-  var interval = 0; // The display interval, in minutes.
+    var interval = 0; // The display interval, in minutes.
 
-  setInterval(function() {
-    interval++;
+    setInterval(function() {
+        interval++;
 
-    if (
-      JSON.parse(localStorage.isActivated) &&
-        localStorage.frequency <= interval
-    ) {
-      show();
-      interval = 0;
-    }
-  }, 60000);
+        if (
+            JSON.parse(localStorage.isActivated) &&
+            localStorage.frequency <= interval
+        ) {
+            show();
+            interval = 0;
+        }
+    }, 60000);
 }
--- popup.js
+++ popup.js
@@ -1,6 +1,6 @@
-var APP = function(){
+var APP = function() {
 
-	//singleton
+    //singleton
     if (APP.instance_) {
         return APP.instance_;
     }
@@ -11,55 +11,79 @@ var APP = function(){
 
 APP.prototype = {
 
-	// Funktsioon, mille käivitame alguses
-	init: function(){
-    var htmlValidation = document.getElementById("htmlValidation");
-    var cssValidation = document.getElementById("cssValidation");
-    var action = document.getElementById("action");
-    htmlValidation.addEventListener('click', this.htmlValidation.bind(this));
-    cssValidation.addEventListener('click', this.cssValidation.bind(this));
-    action.addEventListener('click', this.convertPTSizes.bind(this));
-    
-	},
-  htmlValidation: function(event){
-      chrome.tabs.getSelected(null, function(tab) {
-          window.open("https://validator.w3.org/check?uri="+tab.url, '_blank');
-      });
+    // Funktsioon, mille käivitame alguses
+    init: function() {
+        getDataFromFirebase();
+        var htmlValidation = document.getElementById("htmlValidation");
+        var cssValidation = document.getElementById("cssValidation");
+        var action = document.getElementById("action");
+        htmlValidation.addEventListener('click', this.htmlValidation.bind(this));
+        cssValidation.addEventListener('click', this.cssValidation.bind(this));
+        action.addEventListener('click', this.convertPTSizes.bind(this));
 
-  },
-
-  cssValidation: function(event){
-    chrome.tabs.getSelected(null, function(tab) {
-        window.open("https://jigsaw.w3.org/css-validator/validator?uri="+tab.url, '_blank');
-    });
-  },
-  convertPTSizes: function(event){
-    var ptSize = document.getElementById("fromPT").value;
-    var answerArea = document.getElementById("answer");
-    var answer;
-
-    if (document.getElementById("toPX").checked) {
-           answer = ptSize * (96 / 72) + "px";
-       }
-       else if (document.getElementById("toEM").checked) {
-           answer = ptSize * (1 / 12) + "em";
-       }
-       else if (document.getElementById("toPERCENT").checked) {
-           answer = ptSize * (100 / 12) + "%";
-       }
-       else {
-
-           }
+    },
+    htmlValidation: function(event) {
+        if (user) {
+            chrome.tabs.getSelected(null, function(tab) {
+                writeValidationData(tab.url);
+                console.log(tab.url);
+                window.open("https://validator.w3.org/check?uri=" + tab.url, '_blank');
+            });
+        } else {
+            getUserName();
+            chrome.tabs.getSelected(null, function(tab) {
+                writeValidationData(tab.url);
+                console.log(tab.url);
+                window.open("https://validator.w3.org/check?uri=" + tab.url, '_blank');
+            });
+        }
 
 
+    },
 
-      answerArea.innerHTML = answer;
-  }
+    cssValidation: function(event) {
+
+        if (user) {
+            chrome.tabs.getSelected(null, function(tab) {
+                writeValidationData(tab.url + (' - (HTML)'));
+                chrome.tabs.getSelected(null, function(tab) {
+                    window.open("https://jigsaw.w3.org/css-validator/validator?uri=" + tab.url, '_blank');
+                });
+            });
+        } else {
+            getUserName();
+            chrome.tabs.getSelected(null, function(tab) {
+                writeValidationData(tab.url + (' - (CSS)'));
+                chrome.tabs.getSelected(null, function(tab) {
+                    window.open("https://jigsaw.w3.org/css-validator/validator?uri=" + tab.url, '_blank');
+                });
+            });
+        }
+    },
+    convertPTSizes: function(event) {
+        var ptSize = document.getElementById("fromPT").value;
+        var answerArea = document.getElementById("answer");
+        var answer;
+
+        if (document.getElementById("toPX").checked) {
+            answer = ptSize * (96 / 72) + "px";
+        } else if (document.getElementById("toEM").checked) {
+            answer = ptSize * (1 / 12) + "em";
+        } else if (document.getElementById("toPERCENT").checked) {
+            answer = ptSize * (100 / 12) + "%";
+        } else {
+
+        }
+
+
+
+        answerArea.innerHTML = answer;
+    }
 };
 
 
-window.onload = function(){
-	var chromeExtension = new APP();
-	window.chromeExtension = chromeExtension;
+window.onload = function() {
+    var chromeExtension = new APP();
+    window.chromeExtension = chromeExtension;
 
 };
