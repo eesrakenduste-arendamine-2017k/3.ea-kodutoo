55112f6c933b144ae9dc8fba6f89f931070ab934
Rando Tomingas <randotm@users.noreply.github.com>
2017-05-31 16:07:06 +0300

    Now saving to database with article names as ids, doesn't duplicate values, extra check left in as a comment in code though.

--- background.js
+++ background.js
@@ -11,36 +11,53 @@ $("a").hover(
 		isHovered = true;
 		check = setInterval(function() {
 			if (buttonPressed && isHovered) {
-				//console.log("Salvestamisekoht");
+				//console.log("Ready to save");
+				if (hovered.currentTarget.innerText != "") {
 
-				var id = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-			        var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
-			        return v.toString(16);
-			    });
-				
-			    firebase.database().ref('news/' + id).set({     title:
-			    hovered.currentTarget.innerText,     url:
-			    hovered.currentTarget.href,     date: new Date() });
+					var id = hovered.currentTarget.innerText.replace(/[.#$\[\]]/g," ");
 
+				    firebase.database().ref('news/' + id).set({
+				    	title: hovered.currentTarget.innerText,
+				    	url: hovered.currentTarget.href,
+				    	date: new Date()
+				    });
+					
+					/*
+					var exists = true;
+					var starCountRef = firebase.database().ref('news/' + id);
+					starCountRef.on('value', function(snapshot) {
+						if (snapshot.val() == null) {
+							exists = false;
+						}
+					});
+					if(!exists) {
+					    firebase.database().ref('news/' + id).set({
+					    	title: hovered.currentTarget.innerText,
+					    	url: hovered.currentTarget.href,
+					    	date: new Date()
+					    });
+					}
+					*/
+				}
 			}
 		}, 10);
-		//console.log("Läksin peale");
+		//console.log("Started hovering the link");
 		//console.log(hovered.currentTarget.href);
 		//console.log(hovered.currentTarget.innerText);
 	},function() {
 		isHovered = false;
 		clearInterval(check);
-		//console.log("Lahkusin");
+		//console.log("Left the link");
 	})
 $(document).keydown(function(pressed) {
 	if (pressed.which == 89) {
 		buttonPressed = true;
-		//console.log("Vajutasid y'it");
+		//console.log("y keydown");
 	}
 })
 $(document).keyup(function(pressed) {
 	if (pressed.which == 89) {
 		buttonPressed = false;
-		//console.log("Lasid y üles");
+		//console.log("y keyup");
 	}
 })
