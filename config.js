a4b6c4501b2ae4bd5729231d580034ff6b0a70ac
Richard Kajaste <rkajaste@users.noreply.github.com>
2017-06-01 20:22:10 +0300

    merge

---
+++ config.js
@@ -0,0 +1,8 @@
+var config = {
+    apiKey: "AIzaSyBMjYrjZvycI79veZ7tqfrBwr2eTLEcyZU",
+    authDomain: "customizer-3dea4.firebaseapp.com",
+    databaseURL: "https://customizer-3dea4.firebaseio.com",
+    projectId: "customizer-3dea4",
+    storageBucket: "customizer-3dea4.appspot.com",
+    messagingSenderId: "865282186344"
+};
