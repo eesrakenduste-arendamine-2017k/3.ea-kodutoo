96fabeac6e31f3bf93f18717616ef017f34f4641
Richard Kajaste <rkajaste@users.noreply.github.com>
2017-06-01 22:20:06 +0300

    removed config.js

--- config.js
+++
@@ -1,8 +0,0 @@
-var config = {
-    apiKey: "AIzaSyBMjYrjZvycI79veZ7tqfrBwr2eTLEcyZU",
-    authDomain: "customizer-3dea4.firebaseapp.com",
-    databaseURL: "https://customizer-3dea4.firebaseio.com",
-    projectId: "customizer-3dea4",
-    storageBucket: "customizer-3dea4.appspot.com",
-    messagingSenderId: "865282186344"
-};
