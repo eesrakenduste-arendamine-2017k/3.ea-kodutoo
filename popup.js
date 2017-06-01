a4f808c7814eb7f62ab5aad3e94f22b07cb5484c
Richard Kajaste <rkajaste@users.noreply.github.com>
2017-06-01 23:56:26 +0300

    added firebase to popup, fixed html

--- popup.js
+++ popup.js
@@ -1,28 +1,36 @@
-var fonts = [];
+var font = {};
 
 function getFont(){
     console.log("loaded");
-    var font = {type: null, family: null, color: null};
+    font = {type: null, family: null, color: null};
     font.type = document.getElementById("type").value;
     font.family = document.getElementById("family").value;
     font.color = document.getElementById("color").value;
-    fonts[fonts.length] = font;
+}
+function getBackground(){
+    window.bgcolor = document.getElementById("bgcolor").value();
+    window.bgfile = document.getElementById("bgfile").value();
+}
+function save_to_db(){
     var id = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
         var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
         return v.toString(16);
     });
     firebase.database().ref('webpages/' + id).set({
-        webpage: window.location.href,
         type: font.type,
         family: font.family,
-        color: font.color
+        color: font.color,
+        bgcolor: bgcolor,
+        bgfile: bgfile
     });
-}
 
-window.onload = function(){
+}
+document.addEventListener('DOMContentLoaded', function() {
     console.log("loaded2");
     firebase.initializeApp(config);
     document.getElementById("saveFont").addEventListener("click", getFont);
-};
+    document.getElementById("save").addEventListener("click", getBackground);
+    document.getElementById("apply").addEventListener("click", save_to_db);
+});
 
 
