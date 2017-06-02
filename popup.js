b2d990c6b546fc82310bccb26081ed2a4be13aeb
anuxyo <anusada@greeny.cs.tlu.ee>
2017-06-03 02:11:39 +0300

    added saving and loading functiona etc

--- popup.js
+++ popup.js
@@ -1,1 +1,33 @@
-test
+function saveLinks() {
+	var linksContainer = "";
+	// Et kõiki linke korraga ei võtaks, splitin iga reavahetuse või tühikuga
+	//https://stackoverflow.com/questions/25218677/javascript-split-function-to-split-into-array-at-new-line-or-white-space
+    var links = document.getElementById('links').value.split(/\s+/);
+    for (i=0; i<links.length; i++) {
+		//salvestamine
+		if(links[i] !== ' ') {
+			//lisab salvestatud listile reavahetuse
+			linksContainer += links[i] + '\n';
+			localStorage.links = linksContainer;
+		} else {
+			localStorage.links = linksContainer;
+		}
+    }
+}
+
+function loadLinks() {
+	var links = document.getElementById('links').value.split(/\s+/);
+    for(var i=0; i<links.length; i++){
+
+		// nö puhastab urlid - võtab ära ebavajalikud vahed
+		//https://stackoverflow.com/questions/6623231/remove-all-white-spaces-from-text
+		onlyUrl = links[i].replace(/\s/g, '');
+
+		// avab lehe ja kui väli on tühi, siis annab teate
+		if(onlyUrl !== '') {
+			chrome.tabs.create({"url": "https://"+onlyUrl, "selected": false}); 
+		} else {
+         document.getElementById('links').innerHTML = "You haven't inserted any url's yet... :( ";
+		}
+    }
+}
