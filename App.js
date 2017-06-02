ee27f163cffc44fe7bb461a3f09cd362d1f07028
KristerTarnamaa <KristerTarnamaa@users.noreply.github.com>
2017-06-02 12:18:41 +0300

    Create App.js

---
+++ App.js
@@ -0,0 +1,13 @@
+<script src="https://www.gstatic.com/firebasejs/4.1.1/firebase.js"></script>
+<script>
+  //Lülitab sisse firebase-i
+  var config = {
+    apiKey: "AIzaSyDtscYqIEfjrTG4NamAYdZjBXXVDEIIf9g",
+    authDomain: "ea-kodutoo.firebaseapp.com",
+    databaseURL: "https://ea-kodutoo.firebaseio.com",
+    projectId: "ea-kodutoo",
+    storageBucket: "ea-kodutoo.appspot.com",
+    messagingSenderId: "763032688950"
+  };
+  firebase.initializeApp(config);
+</script>
