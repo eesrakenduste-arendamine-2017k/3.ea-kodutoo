d5f2cc5e08e0bcdd3c687e803e2d6e693f71219a
MK <mrxkollo@users.noreply.github.com>
2017-06-02 22:35:53 +0300

    Koodi automaatne kopeerimine clipboardi Stackoverflowst topelt-hiirevajutusega. (Poolik)

--- background.js
+++ background.js
@@ -3,8 +3,9 @@ function TimeManagement() {
 	this.weekdays = ["pühapäev", "esmaspäev", "teisipäev", "kolmapäev", "neljapäev", "reede", "laupäev"];
 	this.months = ["jaanuar", "veebruar", "märts", "april", "mai", "juuni", "juuli", "august", "september", "oktoober", "november", "detsember"];
 	this.time = 0;
-	this.init();
 
+	if(window.location.hostname === "stackoverflow.com"){this.copy_code();}
+	this.init();
 }
 
 
@@ -14,11 +15,11 @@ TimeManagement.prototype = {
 
 		firebase.initializeApp(config);
 		firebase.database().ref("websites/" + id).set({
+			"time"      : new Date().getTime(),
 			"url"       : window.location.hostname,
 			"protocol"  : window.location.protocol,
 			"time_spent": this.time
 		});
-
 	},
 
 	init: function() {
@@ -28,7 +29,7 @@ TimeManagement.prototype = {
 		var tiktok = setInterval(function() {
 			if(document.hidden == false){
 				that.time++;
-				console.log(that.time);
+				//console.log(that.time);
 			}
 		}, 1000);
 
@@ -40,18 +41,35 @@ TimeManagement.prototype = {
 
 		// Teeb algse salvestuse.
 		this.save_to_database(id);
-		console.log("Made initial save.");
+		//console.log("Made initial save.");
 
 		// Uuendab kirjet iga kolme sekundi tagant.
+		// Trükib välja vastava teate konsooli.
 		var db = firebase.database();
-		var save_data = setInterval(function() {db.ref("/websites/" + id).update({ "time_spent": master.time});}, 3000);
-		console.log("Updated data in the database");
+		var save_data = setInterval(function() {
+			db.ref("/websites/" + id).update({"time_spent": master.time});
+			//console.log("Updated data in the database");
+		}, 3000);
 
 		// Lehe kinni panemisel, viisakalt võtab välja kõik intervalid. Igaksjuhuks.
 		window.onunload = function() {
 			clearInterval(tiktok);
 			clearInterval(save_data);
-			console.log("Cleared intervals");
+			//console.log("Cleared intervals");
+		}
+	},
+
+	copy_code: function(){
+		var code_sections = document.getElementsByClassName("default prettyprint prettyprinted");
+
+		for(var i=0; i<code_sections.length; i++){
+			code_sections[i].addEventListener("dblclick", function() {
+				var node = document.createElement("textarea");
+				node.setAttribute("value", code_sections[this.i].textContent);
+				node.select();
+				document.execCommand("copy");
+				console.log("Data copied.")
+			}.bind(this, false));
 		}
 	}
 };
