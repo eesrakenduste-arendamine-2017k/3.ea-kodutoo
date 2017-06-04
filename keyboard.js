2e3aa82b4d63011db4bc3477e83baeef0fedd4cd
Vlad Minajev <vladminajev@MacBook-Pro-Vlad.local>
2017-06-04 18:39:42 +0300

    Finished. Statistics available

--- keyboard.js
+++ keyboard.js
@@ -1,75 +1,119 @@
 console.log('Extension loaded . . .');
+//var timeout = setTimeout(dataFlow, 3000);
+console.log('Timer starts . . .');
 // console.log(document.getElementsByTagName("body")[0]);
 
-
-//variables
-var helper = [];
-var counter = [];
-var keys;
-var state = false;
-var data;
-var e;
-var nr;
 
 //kuulame klahvi vajutusi
 window.captureEvents(Event.KEYPRESS);
 window.onkeypress = pressed;
+console.log('Key press event . . .');
+
+
+var data = null;
+var count;
+var letters;
+var keys = null;
+var helpC = [];
+var counter = [];
+
+var pressedLetters = [];
+
+var timeout;
+var state;
+
+console.log('Variables declared');
+//teeme massiii kuhu paneme vajutused, ja iga **aja p2rast salvestame andmebaasi
+
+
 
 //database
 var database = firebase.database();
 var ref = database.ref('statistics');
 console.log('DB variables declared');
 
-// ref.on('value', writeUserData, errData);
-// console.log('Ref.on');
+//ref.once('value', getData, errData);
+console.log('Ref.on');
+
+
+
+//load data
+function getData(data) {
+
+    letters = data.val();
+    updateCount();
+
+}
+
+function errData(err){
+  console.log('Error!');
+  console.log(err);
+}
 
 
 function pressed(e) {
-  var button = e.key;
-  state = false;
-  // var letters = data.val();
-  // keys = Object.keys(letters);
-  // for (var i = 0; i < keys.length; i++) {
-  //   if(e.key == keys[i]){
-  //     var k = keys[i];
-  //     var count = letters[k].count;
-  //     firebase.database().ref('statistics/' + e.key).update({
-  //       count: count
-  //   });
-  //   state = true;
-  //   }
-  // }
-  // if(state == false){
-  //
-  // }
-  var letters = ref.on('value', function(datasnapshot) {
+    // console.log('counter '+counter.length);
+    console.log('Key pressed start . . .');
 
-        // console.log(datasnapshot.val());
-        data = datasnapshot.val();
-        keys = Object.keys(data);
-        for(var i = 0; i < keys.length; i++){
-          if(button == keys[i]){
-            console.log('Old key');
-            nr = i + 1;
-            firebase.database().ref('statistics/' + e.key).update({
-              count: nr
-          });
-          state = true;
-          }
-        }
-        if(state !== true){
-          console.log('new key');
+    pressedLetters.push(e.key);
 
-          firebase.database().ref('statistics/' + e.key).update({
-                count: 1
-            });
-        }
-        // var count = data[keys[nr]].count;
-        console.log(keys);
+    if(timeout){
+      clearTimeout(timeout);
+    }
 
+    timeout = setTimeout(getDataFromDb, 3000);
+
+    // state = false;
+    // hakkame kontrollima, kas on sama t2ht
+
+
+    //console.log('Key press END . . .');
+}
+
+function getDataFromDb(){
+  ref.once('value', getData, errData);
+}
+
+function updateCount(){
+
+  console.log('saving... ', pressedLetters);
+
+  for(var i = 0; i < pressedLetters.length; i++){
+    var pressed = pressedLetters[i];
+    pressed = pressed.toString();
+    if (letters && letters[pressed.hashCode()]) {
+
+      letters[pressed.hashCode()].count++;
+
+      count = letters[pressed.hashCode()].count;
+
+    } else {
+      count = 1;
+      if(!letters){letters = [];}
+      letters[pressed.hashCode()] = {
+        key: pressed,
+        count: count
+      };
+    }
+
+    database.ref('statistics/' + pressed.hashCode()).set({
+        key: pressed,
+        count: count
     });
-  // console.log(letters);
 
+  }
 
+  pressedLetters = [];
 
 }
+
+String.prototype.hashCode = function(){
+	var hash = 0;
+	if (this.length === 0) return hash;
+	for (i = 0; i < this.length; i++) {
+		char = this.charCodeAt(i);
+		hash = ((hash<<5)-hash)+char;
+		hash = hash & hash; // Convert to 32bit integer
+	}
+	return hash;
+};
