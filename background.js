c65ce4a251970b3930ecb2908609189779fdfe35
KristerTarnamaa <KristerTarnamaa@users.noreply.github.com>
2017-06-12 13:24:08 +0300

    Töötava lingiga rakendus
    Puudub disaini osa

--- background.js
+++ background.js
@@ -31,23 +31,79 @@ var request = createCORSRequest("get", "http://www.novelupdates.com/rss.php?uid=
 if ( request ){
 // Define a callback function
 	request.onload = function(){
-
-		raw.innerHTML = request.responseText;
+	    var update = xmlToJson(StringToXML(request.responseText));
 		
+		raw.innerHTML = request.responseText;
+		/*
 		var x = document.createElement('div');
 		x.innerHTML =raw.innerHTML;
 		var descriptions = x.getElementsByTagName('description');
 		var links = x.getElementsByTagName('link');
 		var titles = x.getElementsByTagName('title');
-		var pubDate = x.getElementsByTagName('pubDate');
+		var pubDate = x.getElementsByTagName('pubDate');*/
 
-		for (var i=1; i<8; i++){
-			document.getElementById('titles').innerHTML += "<b>"+titles[i].textContent+"</b><br>"+  //küsi miks <a href='"'+links[i]......ei töödanud
-															""+descriptions[i].textContent+""+"<br>"+
-															pubDate[i].textContent+"<br>";
+		for (var i=0; i<8; i++){
+
+			var link = update.rss.channel.item[i].link["#text"];
+			var pubDate = update.rss.channel.item[i].pubDate["#text"].slice(-0,-5);
+			var title = update.rss.channel.item[i].title["#text"];
+		
+			document.getElementById('titles').innerHTML += "<a href='"+link+"' target='"+"_blank"+"'>"+"<b>"+title+"</b>"+"</a>"+
+															""+"<br>"+
+															pubDate+"<br>";
 			
 		}
 	};
 request.send();
 
 }
+
+function StringToXML(oString) {
+ //code for IE
+ if (window.ActiveXObject) { 
+ var oXML = new ActiveXObject("Microsoft.XMLDOM"); oXML.loadXML(oString);
+ return oXML;
+ }
+ // code for Chrome, Safari, Firefox, Opera, etc. 
+ else {
+ return (new DOMParser()).parseFromString(oString, "text/xml");
+ }
+}
+
+function xmlToJson(xml) {
+	
+	// Create the return object
+	var obj = {};
+
+	if (xml.nodeType == 1) { // element
+		// do attributes
+		if (xml.attributes.length > 0) {
+		obj["@attributes"] = {};
+			for (var j = 0; j < xml.attributes.length; j++) {
+				var attribute = xml.attributes.item(j);
+				obj["@attributes"][attribute.nodeName] = attribute.nodeValue;
+			}
+		}
+	} else if (xml.nodeType == 3) { // text
+		obj = xml.nodeValue;
+	}
+
+	// do children
+	if (xml.hasChildNodes()) {
+		for(var i = 0; i < xml.childNodes.length; i++) {
+			var item = xml.childNodes.item(i);
+			var nodeName = item.nodeName;
+			if (typeof(obj[nodeName]) == "undefined") {
+				obj[nodeName] = xmlToJson(item);
+			} else {
+				if (typeof(obj[nodeName].push) == "undefined") {
+					var old = obj[nodeName];
+					obj[nodeName] = [];
+					obj[nodeName].push(old);
+				}
+				obj[nodeName].push(xmlToJson(item));
+			}
+		}
+	}
+	return obj;
+};
