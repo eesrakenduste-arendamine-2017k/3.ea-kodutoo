4f6f73fda6f5ea7ca0703f8942d3d123d7109c7a
Mariam Reintop <mreintop@tlu.ee>
2017-10-14 13:53:50 +0300

    working ish extension

---
+++ popup.js
@@ -0,0 +1,13 @@
+/*function listen(){
+    navigator.getUserMedia  = navigator.getUserMedia ||
+        navigator.webkitGetUserMedia ||
+        navigator.mozGetUserMedia ||
+        navigator.msGetUserMedia;
+
+
+
+
+    if (navigator.getUserMedia) {
+        navigator.getUserMedia({audio: true}, function(stream) {console.log("kõik ok");}, function(err) {
+            console.log("The following error occurred: " + err.name);
+        });}else{console.log("Not supported!");}}*/
